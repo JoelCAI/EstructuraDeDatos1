2d092a5b464333724a60fcee0dba248c8d714f14
JoelCAI <joelargentina12345@gmail.com>
2022-06-03 15:36:50 -0300

    Agregar archivos de proyecto.

---
+++ EstructuraDeDatos1/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraDeDatos1
+{
+    internal class Persona
+    {
+		private int _documento;
+		private string _nombre;
+		private string _apellido;
+		private DateTime _fechaNacimiento;
+
+		public int Documento
+		{
+			get { return this._documento; }
+			set { this._documento = value; }
+		}
+
+		public string Nombre
+		{
+			get { return this._nombre; }
+			set { this._nombre = value; }
+		}
+
+		public string Apellido
+		{
+			get { return this._apellido; }
+			set { this._apellido = value; }
+		}
+		public DateTime FechaNacimiento
+		{
+			get { return this._fechaNacimiento; }
+			set { this._fechaNacimiento = value; }
+		}
+
+		
+
+		public Persona(int documento,string nombre, string apellido, DateTime fechaNacimiento)
+		{
+
+			this._documento = documento;
+			this._nombre = nombre;
+			this._apellido = apellido;
+			this._fechaNacimiento = fechaNacimiento;
+
+		}
+	}
+}
---
+++ EstructuraDeDatos1/Validador.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraDeDatos1
+{
+    internal class Validador
+    {
+        public static int PedirIntMenu(string mensaje, int min, int max)
+        {
+
+            int valor;
+            bool valido = false;
+            string mensajeMenu = "\n Ingrese un valor entre " + min + " y " + max;
+            string mensajeError = "\n El valor no puede ser vacio y tiene que estar entre el rango del Menu solicitado. ";
+
+            do
+            {
+
+                Console.WriteLine(mensaje);
+                Console.WriteLine(mensajeMenu);
+
+                if (!int.TryParse(Console.ReadLine(), out valor) || valor < min || valor > max)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n");
+                    Console.WriteLine(mensajeError);
+                }
+                else
+                {
+                    valido = true;
+                }
+
+            } while (!valido);
+
+            return valor;
+
+        }
+
+        public static string ValidarStringNoVacio(string mensaje)
+        {
+
+            string opcion;
+            bool valido = false;
+            string mensajeValidador = "\n Puede ser combinación de minúsculas, MAYÚSCULAS y caracteres";
+            string mensajeError = "\n Por favor ingrese un valor no vacio para que pueda continuar. ";
+
+            do
+            {
+
+                Console.WriteLine(mensaje);
+                Console.WriteLine(mensajeValidador);
+
+                opcion = Console.ReadLine();
+
+                if (opcion == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n");
+                    Console.WriteLine(mensajeError);
+                }
+                else
+                {
+                    valido = true;
+                }
+
+            } while (!valido);
+
+            return opcion;
+        }
+
+        public static DateTime ValidarFechaIngresada(string mensaje)
+        {
+            bool ingresoCorrecto;
+            DateTime fechaValida;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(mensaje);
+                Console.WriteLine("\n Ingrese un formato válido.");
+                Console.WriteLine("\n El formato correcto es *dd/mm/aaaa*.");
+                Console.WriteLine("\n También puede ser *dd/mm/aaaa hh:mm:ss*.");
+
+                string ingresoNacimiento = Console.ReadLine();
+
+                ingresoCorrecto = DateTime.TryParse(ingresoNacimiento, out fechaValida);
+
+                if (!ingresoCorrecto)
+                {
+                    continue;
+                }
+               
+
+            } while (!ingresoCorrecto);
+
+            return fechaValida;
+        }
+
+        public static void VolverMenu()
+        {
+            Console.WriteLine("\n Presione cualquier tecla para volver al Menú ");
+            Console.ReadKey();
+        }
+
+    }
+}
